4010580c5fa6f300e343652c5ab55e087add8d21
adarshpheonix2810 <adarshsingh786000@gmail.com>
2025-12-22 16:01:11 +0530

    file rename to document

--- searchengine.cpp
+++ searchengine.cpp
@@ -1,7 +1,7 @@
 #include "./header/searchengine.hpp"
 using namespace std;
-
-int main (int argc, char** argv) {
+//argument count(argc) and argument vector(argv)
+int main (int argc, char** argv) {//argv and argc are how command line arguments are passed to main() in C and C++.
     if(argc !=5){
         cout<<"Wrong number of arguments"<<endl;
     }
