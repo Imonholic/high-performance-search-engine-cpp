1ef0265814a28eab9d8d0b704beb6fc15aadc059
adarshpheonix2810 <adarshsingh786000@gmail.com>
2025-12-23 17:50:52 +0530

    Fix empty file handling in document store

--- src/Document_store.cpp
+++ src/Document_store.cpp
@@ -1,6 +1,26 @@
 #include "Document_store.hpp"
 using namespace std;
-int read_file(int *linecounter,int *maxlength, char *file_name){
-    cout<<"Reading file: "<<file_name<<endl;
+int read_file(int *linecounter,int *maxlength, char *file_name){   
+    FILE *file=fopen(file_name, "r");
+    if(file==NULL){
+        cout<<"Cannot open file: "<<file_name<<endl;
+        return -1;
+    }
+    char *line=NULL;
+    size_t len=0;
+    int current_length;
+    while((current_length=getline(&line, &len, file))!=-1){
+        if(current_length>*maxlength){
+            *maxlength=current_length;
+        }
+        (*linecounter)++;
+    }
+    fclose(file);
+    free(line);
+    
+    if(*linecounter==0 || *maxlength==0){
+        cout<<"File is empty: "<<file_name<<endl;
+        return -1;
+    }
     return 0;
 }
