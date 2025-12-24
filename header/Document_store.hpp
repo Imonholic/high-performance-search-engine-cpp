af76a9a5b56782376d04f49a9419b624bf604bf8
adarshpheonix2810 <adarshsingh786000@gmail.com>
2025-12-24 19:02:40 +0530

    refactor: update header includes

--- header/Document_store.hpp
+++ header/Document_store.hpp
@@ -1,4 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "Map.hpp"
 int read_sizes(int *linecounter,int *maxlength, char *file_name);
+int read_input(Mymap* mymap, char* file_name);
--- header/searchengine.hpp
+++ header/searchengine.hpp
@@ -1,3 +1,5 @@
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 #include "./Document_store.hpp"
+#include "./Map.hpp"
