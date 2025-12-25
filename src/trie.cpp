38a910cc71f19d0b241efd77ff603c9cad20ff3e
adarshpheonix2810 <adarshsingh786000@gmail.com>
2025-12-26 05:24:54 +0530

    completed trie insert function

--- src/trie.cpp
+++ src/trie.cpp
@@ -20,5 +20,27 @@ TrieNode::~TrieNode()
     if(child!=nullptr){
         delete child;
     }
-    
+
 };
+void TrieNode::insert(char* token, int id){
+    if(value ==-1 || value ==token[0]){
+        value = token[0];
+        if(strlen(token)==1){
+            //initialise list
+            return;
+        }
+        else{
+            if(child == nullptr){
+                child = new TrieNode();
+            }
+            child->insert(token+1,id);
+        }
+    }
+    else{
+        if(sibling==nullptr){
+            sibling=new TrieNode();
+        }
+        sibling->insert(token,id);
+    }
+    return ;
+}
