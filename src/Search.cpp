4db71e6d1759993afcbe0623136fa3e5b7e1f5da
adarshpheonix2810 <adarshsingh786000@gmail.com>
2025-12-28 17:59:27 +0530

    added search implementation file

---
+++ src/Search.cpp
@@ -0,0 +1,12 @@
+#include "Search.hpp"
+using namespace std;
+void search(char* token, TrieNode *trie, Mymap *map, int k){
+    cout<<"not implemented yet"<<endl;
+}
+void df(TrieNode* trie){
+    cout<<"not implemented yet"<<endl;
+}
+int tf(char* token, TrieNode* trie){
+    cout<<"not implemented yet"<<endl;
+    return -1;
+}
