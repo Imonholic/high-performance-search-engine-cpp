93d1b50198e89bea1dbaf34cb7b517b6868b79e5
adarshpheonix2810 <adarshsingh786000@gmail.com>
2026-01-02 01:48:34 +0530

    Add searchall feature to df() - display all indexed words when no argument provided

--- src/Search.cpp
+++ src/Search.cpp
@@ -1,64 +1,79 @@
 #include "Search.hpp"
 using namespace std;
 
-void search(char* token, TrieNode *trie, Mymap *map, int k){
-    cout<<"not implemented yet"<<endl;
+void search(char *token, TrieNode *trie, Mymap *map, int k)
+{
+    cout << "not implemented yet" << endl;
 }
 
-void df(TrieNode* trie){
-    char* token2 = strtok(NULL, " \t\n");
-    if(token2 == NULL){
-        cout<<"Error: Missing word. Usage: /df <word>"<<endl;
-        return;
+void df(TrieNode *trie)
+{
+    char *token2 = strtok(NULL, " \t\n");
+    if (token2 != NULL)
+    {
+        int wordlen = strlen(token2);
+        int docCount = trie->dfsearchword(token2, 0, wordlen);
+
+        // Display result with clear message
+        if (docCount == 0)
+        {
+            cout << "Term '" << token2 << "' not found in any document" << endl;
+        }
+        else
+        {
+            cout << "Term '" << token2 << "' appears in " << docCount << " document(s)" << endl;
+        }
     }
-    
-    // Calculate length once for performance
-    int wordlen = strlen(token2);
-    int docCount = trie->dfsearchword(token2, 0, wordlen);
-    
-    // Display result with clear message
-    if(docCount == 0){
-        cout<<"Term '"<<token2<<"' not found in any document"<<endl;
-    } else {
-        cout<<"Term '"<<token2<<"' appears in "<<docCount<<" document(s)"<<endl;
+    else{
+        char* buffer = (char*)malloc(256*sizeof(char));
+        trie->searchall(buffer,0);
+        free(buffer);
     }
 }
 
-int tf(char* token, TrieNode* trie){
+int tf(char *token, TrieNode *trie)
+{
     // Get document ID
-    char* token2 = strtok(NULL, " \t\n");
-    if(token2 == NULL){
-        cout<<"Error: Missing document ID. Usage: /tf <doc_id> <word>"<<endl;
+    char *token2 = strtok(NULL, " \t\n");
+    if (token2 == NULL)
+    {
+        cout << "Error: Missing document ID. Usage: /tf <doc_id> <word>" << endl;
         return -1;
     }
-    
+
     // Validate document ID is a number
     int len = strlen(token2);
-    for(int i=0; i<len; i++){
-        if(!isdigit(token2[i])){
-            cout<<"Error: Document ID must be a number"<<endl;
+    for (int i = 0; i < len; i++)
+    {
+        if (!isdigit(token2[i]))
+        {
+            cout << "Error: Document ID must be a number" << endl;
             return -1;
         }
     }
     int id = atoi(token2);
-    
+
     // Get the word to search
     token2 = strtok(NULL, " \t\n");
-    if(token2 == NULL){
-        cout<<"Error: Missing word. Usage: /tf <doc_id> <word>"<<endl;
+    if (token2 == NULL)
+    {
+        cout << "Error: Missing word. Usage: /tf <doc_id> <word>" << endl;
         return -1;
     }
-    
+
     // Search for the word and get frequency
     int wordlen = strlen(token2);
     int frequency = trie->tfsearchword(id, token2, 0, wordlen);
-    
+
     // Display result with clear message
-    if(frequency == 0){
-        cout<<"Term '"<<token2<<"' not found in document "<<id<<endl;
-    } else {
-        cout<<"Term '"<<token2<<"' appears "<<frequency<<" time(s) in document "<<id<<endl;
+    if (frequency == 0)
+    {
+        cout << "Term '" << token2 << "' not found in document " << id << endl;
     }
-    
+    else
+    {
+        cout << "Term '" << token2 << "' appears " << frequency << " time(s) in document " << id << endl;
+    }
+
     return 0;
 }
